35250b11ce32d825decd8334d409ea2fecff8cce
rainboy <rainboylvx@qq.com>
2022-03-18 14:51:05 +0800

    update judgworkpool

--- judge_server/src/judgeWorkPool.hpp
+++ judge_server/src/judgeWorkPool.hpp
@@ -9,6 +9,8 @@
 
 #pragma once
 
+#include <mutex>
+#include <condition_variable>
 #include <vector>
 #include "judgeQueue.hpp"
 
@@ -35,11 +37,46 @@ public:
         if( ret ) _task_cv.notify_one(); //唤醒一个
         return ret;
     }
-
-
 private:
+    void judgeWork();
     std::mutex mtx; //锁
     std::condition_variable _task_cv; //条件
     std::vector<std::thread> _workPool; //工作线程池
     std::atomic<int> ThrNum; //空闲线程数量
 };
+//默认4个线程
+judgeWorkPool::judgeWorkPool(unsigned int size){
+    for(int i = 0 ;i< size ;++i){
+        _workPool.emplace_back(&judgeWorkPool::judgeWork,this);
+        ThrNum.fetch_add(1); //空闲线程+1
+    }
+}
+
+/**
+ * @desc 执行评测的流程
+ *
+ * 整个评测流程分和两个部分:
+ *
+ * 一: 准备阶段
+ *      是否是支持的评语
+ *      查找题目的位置,判断题目是否存在,并返回题目的相关信息
+ *      创建评测的文件夹
+ *      写入代码
+ *      编译
+ *      写入评测队列,进入评测阶段
+ * 二: 评测阶段
+ */
+
+//阶段一,二都应该从一个队列里取放数据,所以要设计一个队列
+void judgeWorkPool::judgeWork(){
+
+    auto &jq = judge_Queue::get(); //得到judge queue
+    judge_Queue_node jn;
+    bool succ = jq.try_deque(jn); //加入
+    if( succ == false) return ;  //失败
+
+    std::cout << "de judge queue succ" << std::endl;
+
+}
+
+
